751c6883b2b1beeb72cfb86fc4657d73ef18015c
ByrdOfAFeather <ADongre@cocat.local>
2017-09-18 15:07:42 -0400

    Finished purifying database, added a map repersenting biology test scores. Test scores indicateconcentrated areas of higher end test scores, and spread out areas of lower end test scores.

--- Accountability Report/data_analyzation.py
+++ Accountability Report/data_analyzation.py
@@ -8,7 +8,7 @@ from sklearn.preprocessing import LabelEncoder
 import xlrd
 import pandas as pd
 from pandas.tools.plotting import scatter_matrix
-report = xlrd.open_workbook('Databases/acctsumm17.xlsx').sheet_by_index(0)
+report = xlrd.open_workbook('Databases/acctsumm15.xlsx').sheet_by_index(0)
 northwest_schools = build_school_dict(report, 'Northwest') # form = {School: cellnumber}
 northwest_grades = build_grade_dict(report, northwest_schools) # form = {Math, bio, eng, school}
 northwest_dataframe = pd.DataFrame.from_dict(northwest_grades, orient='index')
@@ -24,7 +24,7 @@ sandhills_schools = build_school_dict(report, 'Sandhills')
 sandhills_grades = build_grade_dict(report, sandhills_schools)
 southeast_schools = build_school_dict(report, 'Southeast')
 southeast_grades = build_grade_dict(report, southeast_schools)
-piedmont_schools = build_school_dict(report, 'Piedmont Triad')
+piedmont_schools = build_school_dict(report, 'Piedmont-Triad')
 piedmont_grades = build_grade_dict(report, piedmont_schools)
 
 overall_grades = merge_dicts(northwest_schools, north_central_schools, western_schools, northeast_schools, southwest_schools, sandhills_schools, southeast_schools, piedmont_schools)
@@ -50,21 +50,28 @@ NC_database = NC_database.replace('â€'.decode('utf-8'), 'NaN')
 NC_database = NC_database.replace('†'.decode('utf-8'), 'NaN')
 NC_database = NC_database.replace('1-Yes', 1)
 NC_database = NC_database.replace('2-No', 0)
-NC_database.drop(['Location Address 3 [Public School] 2014-15', 'Location Address 2 [Public School] 2014-15', 'School Name', 'District'], axis=1, inplace=True)
+NC_database = NC_database.replace('1-Regular school', 1)
+NC_database = NC_database.replace('2-Special education school', 2)
+NC_database = NC_database.replace('3-Vocational school', 3)
+NC_database = NC_database.replace('4-Alternative/other school', 4)
+NC_database.drop(['Location Address 3 [Public School] 2014-15', 'Location Address 2 [Public School] 2014-15', 'School Name', 'Location ZIP4 [Public School] 2014-15'], axis=1, inplace=True)
 NC_database['Grades 9-12 Students [Public School] 2014-15'] = NC_database['Grades 9-12 Students [Public School] 2014-15'].replace('NaN', 999999)
 NC_database['Total Students All Grades (Excludes AE) [Public School] 2014-15'] = NC_database['Total Students All Grades (Excludes AE) [Public School] 2014-15'].replace('NaN', 999999)
-NC_database['Location ZIP4 [Public School] 2014-15'] = NC_database['Location ZIP4 [Public School] 2014-15'].replace('NaN', 999999)
 NC_database['Latitude [Public School] 2014-15'] = NC_database['Latitude [Public School] 2014-15'].replace('NaN', 99999)
 NC_database['Longitude [Public School] 2014-15'] = NC_database['Longitude [Public School] 2014-15'].replace('NaN', 99999)
-NC_database.to_csv('Databases/omega.csv')
+CORR_database = NC_database
+new_coloumns = []
+for columns in NC_database.columns.values:
+	new_coloumns.append(columns.replace('-', '').replace(' ', '').replace('[', '').replace(']', '').replace('(', '').replace(')', ''))
+
+NC_database.columns = new_coloumns
+
+NC_database.to_csv('Databases/Purified.csv')
+
 
 ### DATABASE SEPERATIONS ###
-CORR_database = NC_database
-CORR_database = CORR_database.replace('NaN', 999)
-CORR_database = CORR_database.drop(CORR_database[CORR_database['Magnet School [Public School] 2014-15'] == 999].index)
-CORR_database = CORR_database.drop(CORR_database[CORR_database['Charter School [Public School] 2014-15'] == 999].index)
-CORR_database = CORR_database.drop(CORR_database[CORR_database['Title I Eligible School [Public School] 2014-15'] == 999].index)
-CORR_database = CORR_database.drop(CORR_database[CORR_database['Grades 9-12 Students [Public School] 2014-15'] == 999999].index)
+CORR_database = CORR_database.drop(CORR_database[CORR_database['MagnetSchoolPublicSchool201415'] == 999].index)
+CORR_database = CORR_database.drop(CORR_database[CORR_database['CharterSchoolPublic School201415'] == 999].index)
+CORR_database = CORR_database.drop(CORR_database[CORR_database['TitleIEligibleSchoolPublicSchool201415'] == 999].index)
+CORR_database = CORR_database.drop(CORR_database[CORR_database['Grades912StudentsPublicSchool201415'] == 999999].index)
 
-omega = CORR_database.corr()
-print(omega)
