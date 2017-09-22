e0b6781deefaedc72f8e98af0bb16227d1fa0003
ByrdOfAFeather <ADongre@cocat.local>
2017-09-22 15:13:20 -0400

    Added testing code taken from outside sources to learn BaseMapping. Current goal: create a 3D plot with the base image being North Carolina. X and Y -> location, Z -> Test scores.

--- Accountability Report/data_analyzation.py
+++ Accountability Report/data_analyzation.py
@@ -9,7 +9,11 @@ from sklearn.preprocessing import LabelEncoder
 import xlrd
 import pandas as pd
 from pandas.tools.plotting import scatter_matrix
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt 
 from mpl_toolkits.mplot3d import Axes3D
+import shapefile
+from descartes import PolygonPatch
 
 
 overall_dataframe = setup_dicts()[0]
@@ -19,10 +23,16 @@ overall_grades = setup_dicts()[1]
 
 ### DATABASE COMBINATIONS ###
 NC_database = setup_NC_DATAFRAME(overall_grades, overall_dataframe)
+NC_database['Omega'] = list(zip(NC_database.LatitudePublicSchool201415, NC_database.LongitudePublicSchool201415))
+map = Basemap(llcrnrlon=-119,llcrnrlat=22,urcrnrlon=-64,urcrnrlat=49,
+        projection='lcc',lat_1=33,lat_2=45,lon_0=-95)
+fig = plt.figure()
+ax = Axes3D(fig)
+ax.add_collection3d(map.drawcoastlines(linewidth=0.25))
 
+ax.scatter(NC_database.LatitudePublicSchool201415.astype(float), NC_database.LongitudePublicSchool201415.astype(float), NC_database.Biology.astype(float))
 
-
-
+plt.show()
 
 
 
