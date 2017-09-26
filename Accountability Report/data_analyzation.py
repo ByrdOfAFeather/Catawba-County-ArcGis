1304d983adffef119967d281f250b31a86ee875e
ByrdOfAFeather <ADongre@cocat.local>
2017-09-26 14:31:21 -0400

    Added base for introductory Machine Learning statistics to indulge the idea that perhaps test scores can be analytically predicted, though first tests without polynomial features have been well, roughly 20% accuracy.

--- Accountability Report/data_analyzation.py
+++ Accountability Report/data_analyzation.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from data_transformation_functions import merge_dicts, build_school_dict, build_grade_dict, build_grade_dataframe, setup_dicts, setup_NC_DATAFRAME
+from data_transformation_functions import merge_dicts, build_school_dict, build_grade_dict, build_grade_dataframe, setup_dicts, setup_NC_DATAFRAME, removesection
 import plot_functions as custplt
 import matplotlib.pyplot as plt
 import numpy as np 
 from sklearn import svm
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, PolynomialFeatures
+from sklearn.model_selection import train_test_split 
+from sklearn.linear_model import Ridge
 import pandas as pd
 from pandas.tools.plotting import scatter_matrix
 import matplotlib.pyplot as plt 
@@ -18,12 +20,24 @@ overall_dataframe = setup_dicts()[0]
 overall_grades = setup_dicts()[1]
 
 NC_database = setup_NC_DATAFRAME(overall_grades, overall_dataframe)
-NC_database.to_csv('Databases/current.csv')
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+y = NC_database.Biology.astype(float).values
+X = removesection(NC_database, ['Biology', 'Math', 'English', 'StateNamePublicSchoolLatestavailableyear', 'LocationAddress1PublicSchool201415', 
+	'LocationCityPublicSchool201415', 'LocationZIPPublicSchool201415', 'TitleISchoolStatusPublicSchool201415', 'LowestGradeOfferedPublicSchool201415', 
+	'HighestGradeOfferedPublicSchool201415', 'District', 'Grades912StudentsPublicSchool201415'])
+X_plot_encoder = LabelEncoder()
+X.SchoolNamePublicSchool201415 = X_plot_encoder.fit_transform(X.SchoolNamePublicSchool201415)
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=.8, random_state=225)
 
-# Next steps - graph relation between student teacher raito, test scores, and title 1 eligibility
-
+print(y_train)
+print(y_train.shape)
+k = Ridge(alpha=1)
+s = PolynomialFeatures(2).fit(X_train)
+k.fit(s, y_train)
+print(k.score(s, y_train))
+print(k.coef_)
+plt.scatter(X_train.SchoolNamePublicSchool201415.astype(float), y_train)
+plt.plot(X_train.SchoolNamePublicSchool201415, k.predict(s))
+plt.show()
 
 
 
--- Licensing/README.md
+++
@@ -1,2 +0,0 @@
-# Catawba-County-ArcGis
-Database analysis from 2017 internship with Catawba County IT department 
