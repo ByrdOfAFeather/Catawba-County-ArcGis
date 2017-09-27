68419ed58f1987f4deb0b8fe3ff1511c208226ff
ByrdOfAFeather <ADongre@cocat.local>
2017-09-27 15:32:29 -0400

    Further work on Machine Learning adaptability to the dataset. So far, results have varied. Higher polynomial degrees definitly increased score, at one point with Elsatic Net regression and degree=7, I was even able to achieve a score of 70% for the train set. However, generalization seems to be problamatic, and programatically wrong. The results end up predicting numbers that don't make sense (1+e^10 or somewhere in that area). More work is required here, graphs added to repersent work so far.

--- Accountability Report/data_analyzation.py
+++ Accountability Report/data_analyzation.py
@@ -5,9 +5,11 @@ import plot_functions as custplt
 import matplotlib.pyplot as plt
 import numpy as np 
 from sklearn import svm
-from sklearn.preprocessing import LabelEncoder, PolynomialFeatures
+from sklearn.preprocessing import LabelEncoder, PolynomialFeatures, StandardScaler
 from sklearn.model_selection import train_test_split 
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import ElasticNet
+from sklearn.model_selection import cross_val_score
+from sklearn.dummy import DummyRegressor
 import pandas as pd
 from pandas.tools.plotting import scatter_matrix
 import matplotlib.pyplot as plt 
@@ -25,22 +27,61 @@ X = removesection(NC_database, ['Biology', 'Math', 'English', 'StateNamePublicSc
 	'LocationCityPublicSchool201415', 'LocationZIPPublicSchool201415', 'TitleISchoolStatusPublicSchool201415', 'LowestGradeOfferedPublicSchool201415', 
 	'HighestGradeOfferedPublicSchool201415', 'District', 'Grades912StudentsPublicSchool201415'])
 X_plot_encoder = LabelEncoder()
-X.SchoolNamePublicSchool201415 = X_plot_encoder.fit_transform(X.SchoolNamePublicSchool201415)
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=.8, random_state=225)
 
-print(y_train)
-print(y_train.shape)
-k = Ridge(alpha=1)
-s = PolynomialFeatures(2).fit(X_train)
-k.fit(s, y_train)
-print(k.score(s, y_train))
-print(k.coef_)
-plt.scatter(X_train.SchoolNamePublicSchool201415.astype(float), y_train)
-plt.plot(X_train.SchoolNamePublicSchool201415, k.predict(s))
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=.8, random_state=225)
+print(X_test)
+
+X_train.SchoolNamePublicSchool201415 = X_plot_encoder.fit_transform(X_train.SchoolNamePublicSchool201415)
+schoolname = X_train.SchoolNamePublicSchool201415.values.astype(float)
+X_train = removesection(X_train, ['SchoolNamePublicSchool201415',])
+X_train = X_train.as_matrix()
+ait = StandardScaler().fit_transform(X_train)
+X_train = ait
+def polynomial_regression_function(degree, alpha, x, y, plot_title='Default', x_lbl='Default', y_lbl='Default', file_name='', location=''):
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	k = ElasticNet(alpha=alpha, max_iter=100, positive=True)
+	s = PolynomialFeatures(degree=degree).fit_transform(X_train)
+	k.fit(s, y_train)
+	plt.xlabel(x_lbl)
+	plt.ylabel(y_lbl)
+	plt.title(plot_title)
+	plt.scatter(schoolname, y_train)
+	plt.scatter(schoolname, k.predict(s), color='r')
+	props = dict(boxstyle='round', facecolor='white', alpha=.5)
+	ax.text(0.01, 0.026, 'Score = {}'.format(k.score(s, y)), transform=ax.transAxes, verticalalignment='top', bbox=props)
+	if file_name:
+		if location:
+			plt.savefig('Graphs/{}/{}'.format(location, file_name) + '.png')
+		else:
+			plt.savefig('Graphs/{}'.format(file_name) + '.png')
+	plt.show()
+	return {'score': k.score(s, y), 'model': k, 'polynomialx': s, 'degree':degree}
+
+
+k_dict = polynomial_regression_function(5, 1, X_train, y_train, plot_title='Polynomial model with alpha=1 degree=5', x_lbl='School encoded (each increase in 1, represents a new school)', y_lbl='Biology Percent Passing')
+model = k_dict['model']
+
+X_test.SchoolNamePublicSchool201415 = X_plot_encoder.fit_transform(X_test.SchoolNamePublicSchool201415)
+schoolname = X_test.SchoolNamePublicSchool201415.values.astype(float)
+X_test = removesection(X_test, ['SchoolNamePublicSchool201415',])
+ait = StandardScaler().fit_transform(X_test)
+
+X_test = X_test.as_matrix()
+X_test = PolynomialFeatures(degree=k_dict['degree']).fit_transform(X_test)
+
+plt.scatter(schoolname, y_test)
+plt.scatter(schoolname, model.predict(X_test), color='r')
+dummy = DummyRegressor()
+dummy.fit(X_test, y_test)
+plt.plot(schoolname, dummy.predict(X_test), color='g')
 plt.show()
 
 
 
+cross_validation = cross_val_score(model, X_test, y_test)
+
+print('Accuracy: {}'.format(model.score(X_test, y_test)))
 
 
 
